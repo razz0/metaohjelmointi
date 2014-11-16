cf8f59e9e4341cf3c97dfb3c6620c5b0e5c11218
Mikko Koho <mikko.koho@iki.fi>
2014-11-16 14:10:27 +0200

    Unohtuneet koodiesimerkit mukaan repoon

---
+++ code/foo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+a = 'Hello world!'
+print(a)
+# Hello world!
+
+a = len(a)
+
+print(a)
+# 12
---
+++ code/introspektio.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+a = 42
+
+dir(a)
+
+a.bit_length.__doc__
+
