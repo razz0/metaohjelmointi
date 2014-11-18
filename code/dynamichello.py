34d8e893006d68841dee29a7fde6bf51174d8c89
Mikko Koho <mikko.koho@iki.fi>
2014-11-18 10:39:41 +0200

    selitä compile-exec esimerkki

--- code/dynamichello.py
+++ code/dynamichello.py
@@ -1,5 +1,8 @@
 code_str = 'print("Hello world!")'
 code_obj = compile(code_str, '<string>', 'single')
 
+print(code_obj)
+# <code object <module> at 0x7f06341f2cb0, file "<string>", line 1>
+
 exec(code_obj)
 # Hello world!
